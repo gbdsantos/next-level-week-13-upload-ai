1d68edc990c43a8ae05e081808ac0544434364f9
Guilherme Bezerra <gbsantos.it@gmail.com>
2023-09-11 21:56:40 -0300

    🎨 styles(frontend): create header

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,5 +1,27 @@
+import { Button } from "./components/ui/button";
+import { Separator } from './components/ui/separator';
+
+import { Github } from 'lucide-react';
+
 export function App() {
   return (
-    <h1>Hello World</h1>
+    <div>
+      <div className="px-6 py-3 flex items-center justify-between border-b">
+        <h1 className="text-xl font-bold">upload.ia</h1>
+
+        <div className="flex items-center gap-3">
+          <span className="text-sm text-muted-foreground">
+            Desenvolvido com ❤️ por Guilherme Bezerra no NLW da Rocketseat
+          </span>
+
+          <Separator className="h-6" orientation="vertical" />
+
+          <Button variant="outline">
+            <Github className="w-4 h-4 mr-2" />
+            Github
+          </Button>
+        </div>
+      </div>
+    </div>
   )
 }

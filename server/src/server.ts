3acc6700703bde4516c4d7fb3d0c8a0def1c4d28
Guilherme Bezerra <gbsantos.it@gmail.com>
2023-09-12 21:40:26 -0300

    ✨ feat(backend): implement videos upload endpoint

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,14 +1,16 @@
 import fastify, { } from 'fastify'
 
 import { getAllPromptsRoute } from './routes/get-all-prompts'
+import { uploadVideoRoute } from './routes/upload-video'
 
 const app = fastify()
 
 app.get('/', (request, response) => {
-  response.code(200).send('🚀 HTTP Server Running!')
+  response.status(200).send('🚀 HTTP Server Running!')
 })
 
 app.register(getAllPromptsRoute)
+app.register(uploadVideoRoute)
 
 app.listen({
   port:3333,

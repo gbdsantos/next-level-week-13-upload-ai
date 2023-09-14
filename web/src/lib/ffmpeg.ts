81eba5309b52c0b4e65f2c37daabb6f03c96779e
Guilherme Bezerra <gbsantos.it@gmail.com>
2023-09-13 21:53:44 -0300

    🚚 refactor(frontend): fix import paths for ffmpeg scripts

--- web/src/lib/ffmpeg.ts
+++ web/src/lib/ffmpeg.ts
@@ -1,8 +1,8 @@
-import { FFmpeg } from '@ffmpeg/ffmpeg'
+import { FFmpeg } from '@ffmpeg/ffmpeg';
 
-import coreURL from '../ffmpeg/ffmpeg/ffmepeg-core.js?url'
-import wasmURL from '../ffmpeg/ffmpeg/ffmepeg-core.wasm?url'
-import workerURL from '../ffmpeg/ffmpeg/ffmepeg-worker.js?url'
+import coreURL from '../ffmpeg/ffmpeg-core.js?url'
+import wasmURL from '../ffmpeg/ffmpeg-core.wasm?url'
+import workerURL from '../ffmpeg/ffmpeg-worker.js?url'
 
 let ffmpeg: FFmpeg | null
 

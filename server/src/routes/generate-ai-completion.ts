4050f22eb92c9037ee95626d433e909e44a1fe40
Guilherme Bezerra <gbsantos.it@gmail.com>
2023-09-14 00:50:57 -0300

    ✨ feat(backend): implement response stream

--- server/src/routes/generate-ai-completion.ts
+++ server/src/routes/generate-ai-completion.ts
@@ -1,6 +1,7 @@
 import { FastifyInstance } from "fastify";
 import { z } from "zod";
 
+import { streamToResponse, OpenAIStream } from 'ai';
 import { openai } from '../lib/openai';
 import { prisma } from "../lib/prisma";
 
@@ -32,9 +33,17 @@ export async function generateAICompletionRoute(app: FastifyInstance) {
       temperature,
       messages: [
         { role: 'user', content: promptMessage}
-      ]
+      ],
+      stream: true,
     })
 
-    return response
+    const stream = OpenAIStream(response)
+
+    streamToResponse(stream, reply.raw, {
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS'
+      },
+    })
   })
 }

6d84e895b0ad31d89a4dc1d1b117982054ceee08
Guilherme Bezerra <gbsantos.it@gmail.com>
2023-09-14 01:20:47 -0300

    ♻ refactor(backend): rename variable name expect by vercel ai package for better compatibility

--- server/src/routes/generate-ai-completion.ts
+++ server/src/routes/generate-ai-completion.ts
@@ -10,11 +10,11 @@ export async function generateAICompletionRoute(app: FastifyInstance) {
   app.post('/ia/complete', async (request, reply) => {
     const bodySchema = z.object({
       videoId: z.string().uuid(),
-      template: z.string(),
+      prompt: z.string(),
       temperature: z.number().min(0).max(1).default(0.5),
     })
 
-    const { videoId, template, temperature } = bodySchema.parse(request.body)
+    const { videoId, prompt, temperature } = bodySchema.parse(request.body)
 
     const video = await prisma.video.findUniqueOrThrow({
       where: {
@@ -26,7 +26,7 @@ export async function generateAICompletionRoute(app: FastifyInstance) {
       return reply.status(400).send({error: 'Video transcription was not generated yet.'})
     }
 
-    const promptMessage = template.replace('{transcription', video.transcription)
+    const promptMessage = prompt.replace('{transcription', video.transcription)
 
     const response = await openai.chat.completions.create({
       model: 'gpt-3.5-turbo-16k',
